7641d73dd0afe3fa75018629d866d568f333577a
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-04-22 08:58:05 +0200

    Added lesson 2: REST

---
+++ les2/scripts/myscript.js
@@ -0,0 +1,55 @@
+/*global document, alert, localStorage, FormData, XMLHttpRequest, jQuery, $
+*/
+
+var par1Input = document.getElementById("par1Input");
+var par2Input = document.getElementById("par2Input");
+
+function handleAjaxResponse() {
+  alert(this.responseText + " resultaatcode: " + this.status);
+}
+
+function handleGet() {
+  var par1 = par1Input.value;
+  var par2 = par2Input.value;
+  var req = new XMLHttpRequest();
+  req.addEventListener("load", handleAjaxResponse);
+  req.open("GET", "echo" + "?par1=" + par1 + "&par2=" + par2);
+  req.send();
+}
+
+document.getElementById("getButton").onclick = handleGet;
+
+function handlePost() {
+  var par1 = par1Input.value;
+  var par2 = par2Input.value;
+  var req = new XMLHttpRequest();
+  req.addEventListener("load", handleAjaxResponse);
+  req.open("POST", "echo");
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  req.send("par1=" + par1 + "&par2=" + par2);
+}
+
+document.getElementById("postButton").onclick = handlePost;
+
+function handlePut() {
+  var data = new FormData();
+  data.append("par1", par1Input.value);
+  data.append("par2", par2Input.value);
+  var req = new XMLHttpRequest();
+  req.addEventListener("load", handleAjaxResponse);
+  req.open("PUT", "echo");
+  req.send(data);
+}
+
+document.getElementById("putButton").onclick = handlePut;
+
+function handleDelete() {
+  var par1 = par1Input.value;
+  var par2 = par2Input.value;
+  var req = new XMLHttpRequest();
+  req.addEventListener("load", handleAjaxResponse);
+  req.open("DELETE", "echo" + "?par1=" + par1 + "&par2=" + par2);
+  req.send();
+}
+
+document.getElementById("deleteButton").onclick = handleDelete;
